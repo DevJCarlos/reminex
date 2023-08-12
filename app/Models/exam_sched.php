143c777c054e92a6094a77165331e1daed080a47
Gian9608 <basangianrogel@gmail.com>
2023-08-12 07:39:44 -0700

    kini update

--- app/Models/exam_sched.php
+++ app/Models/exam_sched.php
@@ -7,5 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class exam_sched extends Model
 {
-    use HasFactory;
+    public function period()
+    {
+        return $this->belongsTo(Period::class, 'period_id');
+    }
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class, 'subject_id');
+    }
+
+    public function section()
+    {
+        return $this->belongsTo(Section::class, 'section_id');
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class, 'room_id');
+    }
 }
