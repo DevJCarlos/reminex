72cf9e403ddb5b12cc96ba37d190adbb78e02f16
JC Bongaitan <bongaitan.jc23@gmail.com>
2023-08-14 20:55:10 +0800

    Course model added

---
+++ app/Models/Course.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\ModelClass;
+
+use App\Models\Student;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    public function subject(){
+        return $this->belongsToMany(Subject::class, 'model_classes');
+    }
+
+    public function student(){
+        return $this->belongsToMany(Student::class,'model_classes');
+    }
+
+}
