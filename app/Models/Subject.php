b00d27a036e5077c54349f1f321de7b6653b3d68
JC Bongaitan <bongaitan.jc23@gmail.com>
2023-08-14 20:42:32 +0800

    Subject model added

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\ModelClass;
+use App\Models\Student;
+
 class Subject extends Model
 {
     use HasFactory;
+
+    public function student(){
+        return $this->belongsToMany(Student::class, 'model_classes');
+    }
+    
+    public function course(){
+        return $this->belongsToMany(Course::class, 'model_classes');
+    }
 }
