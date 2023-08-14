f5c28d5e79dc7be63f6fd14f42aaf85ad2e41e3f
JC Bongaitan <bongaitan.jc23@gmail.com>
2023-08-14 20:52:18 +0800

    section model

---
+++ app/Models/Section.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Section extends Model
+{
+    use HasFactory;
+
+    public function subject(){
+        return $this->belongsToMany(Subject::class,'model_classes');
+    }
+
+    public function student(){
+        return $this->belongsToMany(Student::class,'model_classes');
+    }
+
+}
