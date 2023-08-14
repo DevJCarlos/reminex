95f64acd1b91115c0648dc602b43b95cda2d4ddf
JC Bongaitan <bongaitan.jc23@gmail.com>
2023-08-14 21:37:49 +0800

    Home controller Updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Course;
+use App\Models\Subject;
+
 class HomeController extends Controller
 {
     /**
@@ -25,4 +28,31 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    // dri ka sugod
+    public function saveData(Request $request){
+        $request = 'Csv';
+        $data = $this->getDataFromCsv($request);
+
+        $this->saveCourse($data['course']);
+        $this->saveSubject($data['subject']);
+    }
+
+    public function getDataFromCsv($file){
+        $data = [
+            'course',
+            'subject'
+        ];
+        return $data;
+    }
+
+    public function saveCourse($courseData){
+        Course::create($courseData);
+    }
+
+
+    public function saveSubject($subjectData){
+
+    }
+
 }
