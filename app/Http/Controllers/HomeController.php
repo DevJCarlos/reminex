cfb5be547eb14733e2050b1603444db33c49814d
Gian9608 <basangianrogel@gmail.com>
2023-08-19 19:53:20 -0700

    update homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,64 +36,97 @@ class HomeController extends Controller
     }
 
     // dri ka sugod
-    public function saveData(Request $request){
-        $request->validate([
-            'section' => 'required|mimes:csv,txt',
-        ]);
-    
-        if ($request->hasFile('section')) {
-            $file = $request->file('section');
-            
-            $originalFileName = $file->getClientOriginalName();
-            $explodeName = explode(' ', $originalFileName);
-            $fileName = time() . '-' . implode('-', $explodeName);
-            
-            $file->storeAs('listsection', $fileName, 'public');
-    
-            $data = [
-                'name' => $fileName,
-                'url' => Storage::url('listsection/' . $fileName),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ];
-    
-            // SAVE TO DATABASE
-            DB::table('upload_csv_listsections')->insert($data);
-    
-            // get data from csv
-            $dataFromCsv = $this->getDataFromCsv('listsection/' . $fileName);
-    
-            $uniqueSubjects = collect($dataFromCsv['subject'])->unique('code');
-            foreach ($uniqueSubjects as $subjectData) {
-                $this->saveSubject($subjectData);
-            }
+    public function saveData(Request $request)
+{
+    $request->validate([
+        'section' => 'required|mimes:csv,txt',
+    ]);
 
-            $uniqueCourses = collect($dataFromCsv['course'])->unique('name');
-            foreach ($uniqueCourses as $courseData) {
-                $this->saveCourse($courseData);
-            }
+    if ($request->hasFile('section')) {
+        $file = $request->file('section');
 
-            $uniqueSections = collect($dataFromCsv['section'])->unique('name');
-            foreach ($uniqueSections as $sectionData) {
-                $this->saveSection($sectionData);
-            }
+        $originalFileName = $file->getClientOriginalName();
+        $explodeName = explode(' ', $originalFileName);
+        $fileName = time() . '-' . implode('-', $explodeName);
+
+        $file->storeAs('listsection', $fileName, 'public');
+
+        $data = [
+            'name' => $fileName,
+            'url' => Storage::url('listsection/' . $fileName),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+
+        // SAVE TO DATABASE
+        DB::table('upload_csv_listsections')->insert($data);
+
+        // get data from csv
+        $dataFromCsv = $this->getDataFromCsv('listsection/' . $fileName);
+
+        $filteredSubjects = collect($dataFromCsv['subject'])
+            ->reject(function ($subjectData) {
+                return in_array($subjectData['name'], ['Class Section', 'Course Code', 'Course Description', '']);
+            })
+            ->unique('code');
+
+        foreach ($filteredSubjects as $subjectData) {
+            $this->saveSubject($subjectData);
+        }
+
+        $filteredCourses = collect($dataFromCsv['course'])
+        ->reject(function ($courseData) {
+            return empty(trim($courseData['name']));
+        });
+
+        $courseNames = [];
+        $filteredCourses = collect($dataFromCsv['course'])
+            ->reject(function ($courseData) {
+                return empty(trim($courseData['name']));
+            });
     
-            // waw
-            $uniqueTeachers = collect($dataFromCsv['teacher'])->unique('name');
-            foreach ($uniqueTeachers as $teacherData) {
-                $teacherName = $teacherData['name'];
-            
-                
-                if (strpos($teacherName, "\n") === false) {
-                    $this->saveTeacher($teacherData);
+        foreach ($filteredCourses as $courseData) {
+            $courseName = $courseData['name'];
+    
+            // Use regular expression to extract the first word
+            if (preg_match('/^(\S+)/', $courseName, $matches)) {
+                $firstWord = $matches[1];
+    
+                // Check if the course name has already been saved
+                if (!in_array($firstWord, $courseNames)) {
+                    $courseNames[] = $firstWord;
+                    $courseData['name'] = $firstWord;
+                    $this->saveCourse($courseData);
                 }
             }
-            
         }
         
-    
-        return redirect()->back()->with('success', 'File uploaded successfully.');
+
+        $filteredSections = collect($dataFromCsv['section'])
+            ->reject(function ($sectionData) {
+                return empty(trim($sectionData['name']));
+            })
+            ->unique('name');
+
+        foreach ($filteredSections as $sectionData) {
+            $this->saveSection($sectionData);
+        }
+
+        
+        $filteredTeachers = collect($dataFromCsv['teacher'])
+        ->reject(function ($teacherData) {
+            $teacherName = trim($teacherData['name']);
+            return in_array($teacherName, ['', 'Instructor']) || strpos($teacherName, "\n") !== false;})
+            ->unique('name');
+
+        foreach ($filteredTeachers as $teacherData) {
+            $this->saveTeacher($teacherData);
     }
+    }
+
+    return redirect()->back()->with('success', 'File uploaded successfully.');
+}
+
     
 
     
@@ -114,14 +147,11 @@ class HomeController extends Controller
 
     foreach ($csv as $record) {
 
-        if (in_array($record['0'], ['Course Code', 'Class Section', 'Course Description'])){
-            continue;
-        }
-
         $coursecode = $record[0]; // First column sa csv
         $coursedesc = $record[3]; // Fourth column sa csv
         $sectioncourse = $record[2]; // third column
         $teachername = mb_convert_encoding($record[24], 'UTF-8', 'auto');
+        
 
         $subjectData[] = [
             'code' => $coursecode,
@@ -146,7 +176,7 @@ class HomeController extends Controller
         'subject' => $subjectData,
         'course' => $courseData,
         'section' => $sectionData,
-        'teacher' => $teacherData,
+        'teacher' => $teacherData
     ];
     
 }
@@ -155,13 +185,13 @@ class HomeController extends Controller
         Course::create($courseData);
     }
     public function saveSubject($subjectData){
-        Subject::create($subjectData); 
+        Subject::create($subjectData);
     }
     public function saveSection($sectionData){
-        Section::create($sectionData); 
+        Section::create($sectionData);
     }
     public function saveTeacher($teacherData){
-        Teacher::create($teacherData); 
+        Teacher::create($teacherData);
     }
     
 }
