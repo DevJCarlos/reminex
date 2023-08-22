3ef2e9bbb479e9c65fda7c8a4630cbf9020b5717
JC Bongaitan <bongaitan.jc23@gmail.com>
2023-08-22 20:38:26 +0800

    User updates

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
+use Spatie\Permission\Traits\HasRoles;
+
 class RegisterController extends Controller
 {
     /*
@@ -23,6 +25,7 @@ class RegisterController extends Controller
     */
 
     use RegistersUsers;
+    use HasRoles;
 
     /**
      * Where to redirect users after registration.
@@ -64,10 +67,15 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        //add role before creatung user
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        $user->assignRole('admin');
+
+        return $user;
     }
 }
