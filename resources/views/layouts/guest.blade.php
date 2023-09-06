0d218a88e0e9dfa4d901a6751d9f6d688dc8eebf
JC Bongaitan <bongaitan.jc23@gmail.com>
2023-09-06 14:03:55 +0800

    Guest index update

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,15 +14,34 @@
     <link rel="stylesheet" href="{{ asset('css/icheck-bootstrap.min.css') }}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
+
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="shortcut icon" href="{{asset('import/img/photos/ReminExlogolink2.png')}}" />
+
+	<link rel="canonical" href="https://demo-basic.adminkit.io/" />
+
+	<title>ReminEx</title>
+
+	<link href="{{asset('import/css/app.css')}}" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+
 </head>
 <body class="hold-transition">
     <header>
-        @auth
+        {{-- @auth --}}
             @include('layouts.partial.guest-nav')
-        @endauth
+            
+        {{-- @endauth --}}
     </header>
+    
+    <div class="wrapper">
+        @include('layouts.partial.side-nav')
+        @yield('content')
+    </div>
+    
 
-    @yield('content')
+    
 
 
 @vite('resources/js/app.js')
@@ -30,5 +49,34 @@
 <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('js/adminlte.min.js') }}" defer></script>
+
+<!-- 
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row text-muted">
+            <div class="col-6 text-start">
+                <p class="mb-0">
+                    <a class="text-muted" href="https://adminkit.io/" target="_blank"><strong>AdminKit</strong></a> - <a class="text-muted" href="https://adminkit.io/" target="_blank"><strong>Bootstrap Admin Template</strong></a>								&copy;
+                </p>
+            </div>
+            <div class="col-6 text-end">
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <a class="text-muted" href="https://adminkit.io/" target="_blank">Support</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="text-muted" href="https://adminkit.io/" target="_blank">Help Center</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="text-muted" href="https://adminkit.io/" target="_blank">Privacy</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="text-muted" href="https://adminkit.io/" target="_blank">Terms</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer> -->
 </body>
 </html>
---
+++ resources/views/layouts/partial/side-nav.blade.php
@@ -0,0 +1,50 @@
+<nav id="sidebar" class="sidebar js-sidebar">
+    <div class="sidebar-content js-simplebar">
+        <a class="sidebar-brand" href="index.html">
+            <span class="align-middle">
+                <img src="{{asset('import/img/photos/loginlogo.png')}}" alt="Company Name" width="200"></span>
+        </a>
+
+        <ul class="sidebar-nav">
+            <li class="sidebar-header">
+                Pages
+            </li>
+            <li class="sidebar-item active">
+                <a class="sidebar-link" href="index.html">
+                    <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="examSchedule.html">
+                    <i class="align-middle" data-feather="calendar"></i> <span class="align-middle">Exam Schedule</span>
+                </a>
+            </li>
+            <li class="sidebar-header">
+                Request
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="createRequest.html">
+                    <i class="align-middle" data-feather="edit-3"></i> <span class="align-middle">Create Request</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="viewRequest.html">
+                    <i class="align-middle" data-feather="eye"></i> <span class="align-middle">View Requests</span>
+                </a>
+            </li>
+
+            <li class="sidebar-header">
+                Upshots
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="newSchedule.html">
+                    <i class="align-middle" data-feather="calendar"></i> <span class="align-middle">Your New Schedule</span>
+                </a> 
+            </li>
+
+        </ul>
+    </div>
+</nav>
